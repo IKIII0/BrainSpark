c30726d9d91765d7644b5bf7c9c0d13eb5edfa4b
IKIII0 <rifkialsauqy@students.usu.ac.id>
2025-11-30 19:10:14 +0700

    debugging error 500 #3 akhh

--- server/index.js
+++ server/index.js
@@ -1,3 +1,16 @@
 const express = require("express");
-const app = require("./app");
-module.exports = (req, res) => app(req, res);
+const cors = require("cors");
+const api = require("./routes/sparkRoutes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api", api);
+
+app.get("/", (req, res) => {
+  res.send("Hi, Ini Backend Brain Spark!");
+});
+
+module.exports = (req, res) => {
+  app(req, res);
+};
